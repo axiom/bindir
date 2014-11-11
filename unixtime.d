f5da94d4daec97e5525d4b684e596a03376684e2
Johannes Martinsson <johannes@saltside.se>
2014-11-11 01:24:43 +0100

    Work on unixtime.d

--- unixtime.d
+++ unixtime.d
@@ -6,17 +6,24 @@ import std.conv;
  * Print current unix time stamp, or convert command line arguments from unix
  * time stamp into human readable time.
  */
-void main(string[] args) {
+int main(string[] args) {
 	// Handle the simple case first.
 	if (args.length <= 1) {
 		writeln(Clock.currTime().toUnixTime);
-		return;
+		return 0;
 	}
 
-	foreach (input; args[1 .. $]) {
-		immutable unixTime = parse!long(input);
-		immutable t = SysTime(unixTimeToStdTime(unixTime));
-
-		writeln(t.toISOExtString());
+	try {
+		foreach (input; args[1 .. $]) {
+			immutable unixTime = parse!long(input);
+			immutable t = SysTime(unixTimeToStdTime(unixTime));
+			writeln(t.toISOExtString());
+		}
+	} catch (std.conv.ConvException exc) {
+		stderr.writeln("Could not parse input as unix time stamp.");
+		stderr.writeln("Exception: ", exc.msg);
+		return 1;
 	}
+
+	return 0;
 }
